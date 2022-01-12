3e448e95a36cd31fdd0418b4e23bc8518f754aa2
SchiavoneS56 <schiavones22@hsnet.ahsd.org>
2022-01-12 12:14:49 -0500

    Initial commit

---
+++ app/src/main/java/com/schiavone/honorsmobileapps/navigationcomponentlab1/ChooseItemFragment.kt
@@ -0,0 +1,30 @@
+package com.schiavone.honorsmobileapps.navigationcomponentlab1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.schiavone.honorsmobileapps.navigationcomponentlab1.databinding.FragmentChooseItemBinding
+import com.schiavone.honorsmobileapps.navigationcomponentlab1.databinding.FragmentGroceryListBinding
+
+
+class ChooseItemFragment : Fragment() {
+
+    private var _binding: FragmentChooseItemBinding?=null
+    private val binding get()=_binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding= FragmentChooseItemBinding.inflate(inflater, container,false)
+        val rootview = binding.root
+        return rootview
+    }
+    override fun onDestroyView(){
+        super.onDestroyView()
+        _binding=null
+    }
+}
